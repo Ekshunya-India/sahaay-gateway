7d22a5b09d1fbcfcadb15d854950cca8e4946b0b
Sunil KS <sunilks.kavali@gmail.com>
2020-07-20 05:50:15 +0530

    Added the skeletol spring cloud gateway framework.

---
+++ src/main/java/org/ekshunya/sahaaygateway/SahaayGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.ekshunya.sahaaygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SahaayGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SahaayGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ekshunya/sahaaygateway/SahaayGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ekshunya.sahaaygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SahaayGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
